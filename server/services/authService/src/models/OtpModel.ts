34d882c817c43d1f8abfff087962abafda4bb502
aashinak <aashin9605@gmail.com>
2024-11-05 21:18:42 +0530

    create otp model

---
+++ server/services/authService/src/models/OtpModel.ts
@@ -0,0 +1,17 @@
+import { Model, model, Schema } from "mongoose";
+import { IOtp } from "../entities/IOtp";
+const otpSchema: Schema<IOtp> = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  otp: { type: String, required: true }, // Store hashed OTP
+  otpCreatedAt: {
+    type: Date,
+    default: Date.now,
+    expires: 300, // OTP will expire after 5 minutes (300 seconds)
+  },
+});
+
+export const Otp: Model<IOtp> = model<IOtp>("Otp", otpSchema);
