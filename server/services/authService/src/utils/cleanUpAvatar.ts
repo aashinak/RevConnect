a04a517840f08ec0428cf4b27c5102f534d947bb
aashinak <aashin9605@gmail.com>
2024-11-05 21:24:40 +0530

    create avatar clean up helper

---
+++ server/services/authService/src/utils/cleanUpAvatar.ts
@@ -0,0 +1,13 @@
+import logger from "./logger";
+import fs from 'fs/promises'
+// Helper function for avatar cleanup
+async function cleanUpAvatar(avatarLocalPath: string) {
+    try {
+      await fs.unlink(avatarLocalPath);
+      logger.info(`Deleted uploaded avatar: ${avatarLocalPath}`);
+    } catch (err: any) {
+      logger.error(`Failed to delete uploaded avatar: ${err.message}`);
+    }
+  }
+
+export default cleanUpAvatar
