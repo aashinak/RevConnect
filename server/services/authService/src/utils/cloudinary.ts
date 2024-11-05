d79b5bef46d6f199d157e95fa6b3289849fff38c
aashinak <aashin9605@gmail.com>
2024-11-05 21:24:03 +0530

    create cloudinary helper

---
+++ server/services/authService/src/utils/cloudinary.ts
@@ -0,0 +1,17 @@
+import { v2 as cloudinary } from "cloudinary";
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Cloudinary upload function
+const uploadToCloudinary = async (filePath: string, folder: string) => {
+  return cloudinary.uploader.upload(filePath, {
+    folder: folder || "uploads", // Default folder
+  });
+};
+
+export default uploadToCloudinary
