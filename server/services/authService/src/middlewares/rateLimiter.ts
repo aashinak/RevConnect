16fd3464aa74bef3ee1d2f472785a27ae9155ec0
aashinak <aashin9605@gmail.com>
2024-11-05 21:28:55 +0530

    setup rate limiter

---
+++ server/services/authService/src/middlewares/rateLimiter.ts
@@ -0,0 +1,18 @@
+import rateLimit from 'express-rate-limit';
+import logger from '../utils/logger';
+
+// Create a rate limiter
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 8, // Limit each IP to 8 requests per windowMs
+    handler: (req, res) => {
+      logger.warn(`Too many requests detected from IP ::: ${req.ip}`)
+      res.status(429).json({
+        status: 'error',
+        message: 'Too many requests, please try again later.',
+        // limit: 2, // Optional: provide the limit
+      });
+    },
+  });
+
+  export default limiter
