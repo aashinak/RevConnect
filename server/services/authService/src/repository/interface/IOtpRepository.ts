3518b9a93c3727fcfa267dc647836e414a054cf3
aashinak <aashin9605@gmail.com>
2024-11-18 16:58:42 +0530

    prettier updates in Irepo

--- server/services/authService/src/repository/interface/IOtpRepository.ts
+++ server/services/authService/src/repository/interface/IOtpRepository.ts
@@ -1,25 +1,29 @@
 import { IOtp } from "../../entities/IOtp";
 
 export interface IOtpRepository {
-  /**
-   * Save a new OTP or update an existing one.
-   * @param email The user's email
-   * @param otp The generated OTP (already hashed)
-   * @returns The saved OTP document
-   */
-  saveOtp(email: string, otp: string): Promise<IOtp | null>;
+    /**
+     * Save a new OTP or update an existing one.
+     * @param email The user's email
+     * @param otp The generated OTP (already hashed)
+     * @returns The saved OTP document
+     */
+    saveOtp(
+        email: string,
+        otp: string,
+        otpReason: string
+    ): Promise<IOtp | null>;
 
-  /**
-   * Find an OTP by email.
-   * @param email The user's email
-   * @returns The OTP document if found, otherwise null
-   */
-  findOtpByEmail(email: string): Promise<IOtp | null>;
+    /**
+     * Find an OTP by email.
+     * @param email The user's email
+     * @returns The OTP document if found, otherwise null
+     */
+    findOtpByEmail(email: string): Promise<IOtp | null>;
 
-  /**
-   * Delete an OTP after verification.
-   * @param email The user's email
-   * @returns A boolean indicating if deletion was successful
-   */
-  deleteOtp(email: string): Promise<boolean>;
+    /**
+     * Delete an OTP after verification.
+     * @param email The user's email
+     * @returns A boolean indicating if deletion was successful
+     */
+    deleteOtp(email: string): Promise<boolean>;
 }
--- server/services/authService/src/repository/interface/IUserRepository.ts
+++ server/services/authService/src/repository/interface/IUserRepository.ts
@@ -1,9 +1,16 @@
 import IUser from "../../entities/IUser";
 
 export interface IUserRepository {
-  saveUser(user: IUser): Promise<IUser | null>;
-  findUserById(userId: string): Promise<IUser | null>;
-  findUserByEmail(email: string): Promise<IUser | null>;
-  findUserByGoogleId(googleId: string): Promise<IUser | null>;
-  updateUser(userId: string, userUpdate: Partial<IUser>): Promise<IUser | null>;
+    saveUser(user: IUser): Promise<IUser | null>;
+    findUserById(userId: string): Promise<IUser | null>;
+    findUserByEmail(email: string): Promise<IUser | null>;
+    findUserByGoogleId(googleId: string): Promise<IUser | null>;
+    updateUser(
+        userId: string,
+        userUpdate: Partial<IUser>
+    ): Promise<IUser | null>;
+    findOneandUpdate(
+        constraints: Partial<IUser>,
+        userUpdate: Partial<IUser>
+    ): Promise<IUser | null>;
 }
