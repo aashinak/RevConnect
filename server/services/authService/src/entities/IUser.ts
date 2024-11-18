a5c1a30d6975fe424c6a290d7f87b0105210cf99
aashinak <aashin9605@gmail.com>
2024-11-18 16:56:56 +0530

    add more fields to Iuser

--- server/services/authService/src/entities/IUser.ts
+++ server/services/authService/src/entities/IUser.ts
@@ -1,12 +1,15 @@
 export default interface IUser {
+    _id?: string;
     name: string;
     email: string;
     password?: string;
     googleId?: string;
+    provider: string;
     role: string;
     avatar?: string;
     refreshToken?: string;
-    isVerified?: boolean,
+    isVerified?: boolean;
+    isBlocked?: boolean;
     createdAt?: Date;
     updatedAt?: Date;
-  }
+}
