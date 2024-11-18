05eeb3fa95bc8de5b9ed9f27280e8eecc7f96564
aashinak <aashin9605@gmail.com>
2024-11-18 16:57:26 +0530

    update otp model with otp reason field

--- server/services/authService/src/models/OtpModel.ts
+++ server/services/authService/src/models/OtpModel.ts
@@ -1,17 +1,24 @@
 import { Model, model, Schema } from "mongoose";
 import { IOtp } from "../entities/IOtp";
 const otpSchema: Schema<IOtp> = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  otp: { type: String, required: true }, // Store hashed OTP
-  otpCreatedAt: {
-    type: Date,
-    default: Date.now,
-    expires: 300, // OTP will expire after 5 minutes (300 seconds)
-  },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    otp: { type: String, required: true }, // Store hashed OTP
+
+    otpReason: {
+        type: String,
+        required: true,
+        enum: ["password_reset", "email_verification"],
+    },
+    otpCreatedAt: {
+        type: Date,
+        default: Date.now,
+        expires: 300, // OTP will expire after 5 minutes (300 seconds)
+    },
 });
 
 export const Otp: Model<IOtp> = model<IOtp>("Otp", otpSchema);
