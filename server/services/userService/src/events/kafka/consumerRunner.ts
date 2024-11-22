c338ab57ccb3385135a439f5f9d0251268affa69
aashinak <aashin9605@gmail.com>
2024-11-22 14:35:22 +0530

    create consumer runner

---
+++ server/services/userService/src/events/kafka/consumerRunner.ts
@@ -0,0 +1,16 @@
+import logger from "../../utils/logger";
+import  userConsumer  from "./consumers/userConsumer";
+
+const startConsumers = async () => {
+  try {
+    logger.info("Initializing all Kafka consumers...");
+    // Start multiple consumers concurrently
+    await Promise.all([userConsumer(), ]);
+    logger.info("All Kafka consumers are running.");
+  } catch (error: any) {
+    logger.error("Error while initializing Kafka consumers", { error: error.message });
+    process.exit(1); // Exit process if consumers fail to start
+  }
+};
+
+startConsumers();
