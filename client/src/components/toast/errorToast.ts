f0c16acd561189335cd00a44a38125da57c882aa
aashinak <aashin9605@gmail.com>
2024-11-27 10:01:07 +0530

    error toast and constants

---
+++ client/src/components/toast/errorToast.ts
@@ -0,0 +1,15 @@
+// import { toast } from "@/hooks/use-toast";
+
+// export const showTimedToast = () => {
+//     // Display toast and get its ID
+//     const id = toast({
+//       title: "Scheduled: Catch up",
+//       description: "Friday, February 10, 2023 at 5:57 PM",
+//       action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
+//     });
+
+//     // Automatically dismiss the toast after 5 seconds
+//     setTimeout(() => {
+//       toast.remove(id);
+//     }, 5000); // 5000ms = 5 seconds
+//   };
