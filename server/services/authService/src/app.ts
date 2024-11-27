5f283302c5c3016a9c8381e10b9c6970a5a5430c
aashinak <aashin9605@gmail.com>
2024-11-27 11:00:16 +0530

    updated cors

--- server/services/authService/src/app.ts
+++ server/services/authService/src/app.ts
@@ -12,7 +12,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL || "http://localhost:3000",
+        credentials: true,
+    })
+);
 
 // log formating
 const morganFormat: string = ":method :url :status :response-time ms";
